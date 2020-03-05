69431f300289fca5a78e08df9042102814b4fdc2
Hauke Lampe <lampe@hauke-lampe.de>
2020-03-05 04:41:06 +0100

    cleanup

--- app/src/main/java/org/openchaos/android/coverlock/MainActivity.kt
+++ app/src/main/java/org/openchaos/android/coverlock/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import android.view.View
